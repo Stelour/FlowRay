4cd6f14dd4de6ad8ba62a095f8c6e229f73edc79
Stelour <stelourgg@gmail.com>
2026-09-01 23:34:12 +0500

    refactoring

--- socket.cpp
+++ socket.cpp
@@ -2,10 +2,13 @@
 #include "socket.h"
 
 static int send_req(int fd) {
-    struct sockaddr_nl nladdr = {
-        .nl_family = AF_NETLINK
-    };
+    // send to Linux kernel.
+    sockaddr_nl kernel_addr{};
+    kernel_addr.nl_family = AF_NETLINK;
+    kernel_addr.nl_pid = 0;
+    kernel_addr.nl_groups = 0;
 
+    // sock_diag msg
     DiagRequest req{};
     req.nlh.nlmsg_len = sizeof(req);
     req.nlh.nlmsg_type = SOCK_DIAG_BY_FAMILY;
@@ -14,17 +17,17 @@ static int send_req(int fd) {
     req.req.sdiag_protocol = IPPROTO_TCP;
     req.req.idiag_states = 0xFFFFFFFF;
 
-    struct iovec iov = {
-        .iov_base = &req,
-        .iov_len = sizeof(req)
-    };
+    // buffer of msg
+    iovec iov{};
+    iov.iov_base = &req;
+    iov.iov_len = sizeof(req);
 
-    struct msghdr msg = {
-        .msg_name = &nladdr,
-        .msg_namelen = sizeof(nladdr),
-        .msg_iov = &iov,
-        .msg_iovlen = 1
-    };
+    // desc
+    msghdr msg{};
+    msg.msg_name = &kernel_addr;
+    msg.msg_namelen = sizeof(kernel_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
 
     for (;;) {
         if (sendmsg(fd, &msg, 0) < 0) {
@@ -32,11 +35,27 @@ static int send_req(int fd) {
                 continue;
 
             std::cerr << "ERROR: sendmsg" << std::endl;
-            return 2;
+            return 1;
         }
         return 0;
     }
 }
+
+// static int receive_response(int fd) {
+//     alignas(nlmsghdr) char buffer[8192]{};
+//     sockaddr_nl nladdr{};
+//     iovec iov{
+//         .iov_base = buffer,
+//         .iov_len = sizeof(buffer)
+//     };
+//     msghdr msg{
+//         .msg_name = &nladdr,
+//         .msg_namelen = sizeof(nladdr),
+//         .msg_iov = &iov,
+//         .msg_iovlen = 1
+//     };
+//     int flags = 0;
+// }
 
 status_msg socket_req() {
     int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_SOCK_DIAG);
@@ -50,6 +69,10 @@ status_msg socket_req() {
         return status_msg::error;
     }
     std::cout << "request send" << std::endl;
+    // if (receive_response(fd) > 0) {
+    //     close(fd);
+    //     return status_msg::error;
+    // }
     close(fd);
     return status_msg::success;
 }
